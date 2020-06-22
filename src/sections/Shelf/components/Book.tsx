ff862946407ab5b7ae556b66f7fc7039f1d0dbe7
Praveen <pvnsripati@gmail.com>
2020-06-23 00:13:31 +0530

    fix: handleCardClick function redirects to books page when clicked

--- src/sections/Shelf/components/Book.tsx
+++ src/sections/Shelf/components/Book.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, Redirect } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import { Card, Button, Menu, Dropdown } from 'antd';
 import { DownCircleFilled } from '@ant-design/icons';
 import { TypeBook } from '../../../lib/types';
@@ -17,20 +17,25 @@ interface EventObject {
 const { Meta } = Card;
 
 export const Book = ({ book, onMoveBook, showBook }: Props) => {
+  const [clickCard, setClickCard] = useState({ isClicked: false });
+
   const handleCardClick = (event: any) => {
     event.persist();
     if (
-      event.target.localName === 'button' ||
-      event.target.localName === 'path' ||
-      event.target.localName === 'li'
+      !(
+        event.target.localName === 'button' ||
+        event.target.localName === 'path' ||
+        event.target.localName === 'li'
+      )
     ) {
-      return;
-    } else {
+      console.log(event);
+      setClickCard({ isClicked: true });
       showBook(book);
     }
   };
 
   const handleMenuClick = (event: EventObject) => {
+    setClickCard({ isClicked: false });
     switch (event.key) {
       case '1':
         onMoveBook(book, 'currentlyReading');
@@ -55,34 +60,36 @@ export const Book = ({ book, onMoveBook, showBook }: Props) => {
     </Menu>
   );
 
+  const card = (
+    <div className="shelf-books__book" onClick={handleCardClick}>
+      <Card
+        bordered
+        hoverable
+        style={{ width: 240 }}
+        cover={<img alt="example" src={book.imageLinks.thumbnail} />}
+      >
+        <Meta
+          className="book-title-bottom book-description"
+          title={book.title}
+          description={
+            book.subtitle ? book.subtitle : book.description.slice(0, 50)
+          }
+        ></Meta>
+        {/* <span className="book-author-text-color">{book.authors[0]}</span> */}
+        <Dropdown overlay={menu}>
+          <Button
+            className="book-card-move-button"
+            type="primary"
+            shape="circle"
+            icon={<DownCircleFilled />}
+            size="large"
+          ></Button>
+        </Dropdown>
+      </Card>
+    </div>
+  );
+
   return (
-    <Link to="/book">
-      <div className="shelf-books__book" onClick={handleCardClick}>
-        <Card
-          bordered
-          hoverable
-          style={{ width: 240 }}
-          cover={<img alt="example" src={book.imageLinks.thumbnail} />}
-        >
-          <Meta
-            className="book-title-bottom book-description"
-            title={book.title}
-            description={
-              book.subtitle ? book.subtitle : book.description.slice(0, 50)
-            }
-          ></Meta>
-          {/* <span className="book-author-text-color">{book.authors[0]}</span> */}
-          <Dropdown overlay={menu}>
-            <Button
-              className="book-card-move-button"
-              type="primary"
-              shape="circle"
-              icon={<DownCircleFilled />}
-              size="large"
-            ></Button>
-          </Dropdown>
-        </Card>
-      </div>
-    </Link>
+  <div>{JSON.stringify(clickCard.isClicked)}{clickCard.isClicked ? <Redirect push to="/book">{card}</Redirect> : card}</div>
   );
 };
