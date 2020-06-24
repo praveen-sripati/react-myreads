92cb05cb6e27a0aeb8e24d1a3a493bb03f4d0aac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-24 12:48:17 +0000

    Deploy praveen-sripati/react-myreads to github.com/praveen-sripati/react-myreads.git:gh-pages

---
+++ precache-manifest.dd362cdf36813ffd5e36ff5e20627f7f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec9e099fce68e793bad223e80201a15e",
+    "url": "/react-myreads/index.html"
+  },
+  {
+    "revision": "77628dab0167446717d7",
+    "url": "/react-myreads/static/css/2.f47422e6.chunk.css"
+  },
+  {
+    "revision": "e8d461dbfa19a74cdc4c",
+    "url": "/react-myreads/static/css/main.863dbb74.chunk.css"
+  },
+  {
+    "revision": "77628dab0167446717d7",
+    "url": "/react-myreads/static/js/2.0490a7fd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-myreads/static/js/2.0490a7fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d461dbfa19a74cdc4c",
+    "url": "/react-myreads/static/js/main.1333dc17.chunk.js"
+  },
+  {
+    "revision": "3f869e7cce05ea4621c8",
+    "url": "/react-myreads/static/js/runtime-main.a3c3121c.js"
+  },
+  {
+    "revision": "04ae4764882bfa38ef5cb407f250ecc2",
+    "url": "/react-myreads/static/media/No_books.04ae4764.png"
+  },
+  {
+    "revision": "706ab192dca4c4869710d7d41275e35e",
+    "url": "/react-myreads/static/media/searchEmptyState.706ab192.png"
+  }
+]);
